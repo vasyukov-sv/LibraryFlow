e2b70e942f7966891b3218d421aadb70908999da
vasyukov-sv <vasukovsv@gmail.com>
2017-05-08 12:12:50 +0300

    make content book is null

--- src/main/java/ru/javabegin/training/springlibrary/dao/impl/BookDAOImpl.java
+++ src/main/java/ru/javabegin/training/springlibrary/dao/impl/BookDAOImpl.java
@@ -2,6 +2,11 @@ package ru.javabegin.training.springlibrary.dao.impl;
 
 import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.ProjectionList;
+import org.hibernate.criterion.Projections;
+import org.hibernate.transform.Transformers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,22 +25,53 @@ import java.util.List;
 public class BookDAOImpl implements BookDAO {
 
     private final SessionFactory sessionFactory;
+    private ProjectionList bookProjection;
+
     @SuppressWarnings("FieldCanBeLocal")
-    private List<?> books;
+    private List<Book> books;
 
     @Autowired
     public BookDAOImpl(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
+        bookProjection = Projections.projectionList();
+        bookProjection.add(Projections.property("id"), "id");
+        bookProjection.add(Projections.property("name"), "name");
+        bookProjection.add(Projections.property("image"), "image");
+        bookProjection.add(Projections.property("genre"), "genre");
+        bookProjection.add(Projections.property("pageCount"), "pageCount");
+        bookProjection.add(Projections.property("isbn"), "isbn");
+        bookProjection.add(Projections.property("publisher"), "publisher");
+        bookProjection.add(Projections.property("author"), "author");
+        bookProjection.add(Projections.property("publishYear"), "publishYear");
+        bookProjection.add(Projections.property("descr"), "descr");
+        bookProjection.add(Projections.property("rating"), "rating");
+        bookProjection.add(Projections.property("voteCount"), "voteCount");
     }
 
     @Transactional
     @Override
-    public List<?> getBooks() {
-        books = (List<?>) sessionFactory.getCurrentSession()
-                .createCriteria(Book.class)
-                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY)
+    public List<Book> getBooks() {
+        DetachedCriteria bookListCriteria = DetachedCriteria.forClass(Book.class, "b");
+        createAliases(bookListCriteria);
+        //        books = (List<?>) sessionFactory.getCurrentSession()
+//                .createCriteria(Book.class)
+//                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY)
+//                .list();
+        return createBookList(bookListCriteria);
+    }
+
+    private List<Book> createBookList(DetachedCriteria bookListCriteria) {
+        Criteria criteria = bookListCriteria.getExecutableCriteria(sessionFactory.getCurrentSession());
+        return criteria.addOrder(Order.asc("b.name"))
+                .setProjection(bookProjection)
+                .setResultTransformer(Transformers.aliasToBean(Book.class))
                 .list();
-        return books;
+    }
+
+    private void createAliases(DetachedCriteria criteria) {
+        criteria.createAlias("b.author", "author");
+        criteria.createAlias("b.genre", "genre");
+        criteria.createAlias("b.publisher", "publisher");
     }
 
     @Override
